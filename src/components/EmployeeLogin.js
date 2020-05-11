4124a70caf169397dc4bb73586b9bf21c12a5330
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-11 15:44:31 -0700

    Adding MyTaks.js file

--- src/components/EmployeeLogin.js
+++ src/components/EmployeeLogin.js
@@ -24,7 +24,7 @@
 //   render() {
 //     return (
 //       <form onSubmit={this.handleSubmit}>
-//         <label>Supervisor Login:   </label>
+//         <label>Employee Login:   </label>
 //         <input placeholder="email" value={this.state.email} name="email" type="text" onChange={this.handleChange} />
 //         <input placeholder="password" value={this.state.password} name="password" type="text" onChange={this.handleChange} />
 //         <input type="submit" value="Log In" />
