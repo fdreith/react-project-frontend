6690c3ee76e91f71413d0895a84823923c6e4a3a
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-11 14:20:57 -0700

    switching from function to class to function to class

--- src/components/EmployeeLogin.js
+++ src/components/EmployeeLogin.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
-// import { connect } from 'react-redux'
 import { login } from '../actions/currentEmployee'
+import { connect } from 'react-redux'
 
 
 class EmployeeLogin extends Component {
-  constructor() {
-    super()
-    this.state = {
-      email: "",
-      password: ""
-    }
 
+  state = {
+    email: "",
+    password: ""
   }
 
   handleChange = event => {
@@ -18,8 +15,6 @@ class EmployeeLogin extends Component {
       [event.target.name]: event.target.value
     })
   }
-
-
 
   handleSubmit = event => {
     event.preventDefault()
@@ -29,14 +24,57 @@ class EmployeeLogin extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>Employee Login:   </label>
-        <input placeholder="email" type="text" name="email" onChange={this.handleChange} value={this.state.email} />
-        <input placeholder="password" type="text" name="password" onChange={this.handleChange} value={this.state.password} />
+        <label>Supervisor Login:   </label>
+        <input placeholder="email" value={this.state.email} name="email" type="text" onChange={this.handleChange} />
+        <input placeholder="password" value={this.state.password} name="password" type="text" onChange={this.handleChange} />
         <input type="submit" value="Log In" />
       </form>
     )
   }
 
-}
-export default EmployeeLogin
 
+}
+export default connect(null, { login })(EmployeeLogin)
+
+
+
+// import React from 'react'
+// import { login } from '../actions/currentEmployee'
+// import { updateLoginForm } from "../actions/loginForm.js"
+// import { connect } from 'react-redux'
+
+// const EmployeeLogin = props => {
+
+
+//   const handleChange = event => {
+//     const updatedFormData = {
+//       ...props.loginFormData,
+//       [event.target.name]: event.target.value
+//     }
+//     updateLoginForm(updatedFormData)
+//   }
+
+//   const handleSubmit = event => {
+//     event.preventDefault()
+//     login(props.loginFormData)
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>Employee Login:   </label>
+//         <input placeholder="email" value={props.loginFormData.email} type="text" name="email" onChange={handleChange} />
+//         <input placeholder="password" type="text" name="password" onChange={handleChange} value={props.loginFormData.password} />
+//         <input type="submit" value="Log In" />
+//       </form>
+//     </div>
+//   )
+
+// }
+// const mapStateToProps = state => {
+//   return {
+//     loginFormData: state.loginForm
+//   }
+// }
+
+// export default connect(mapStateToProps, { updateLoginForm, login })(EmployeeLogin)
