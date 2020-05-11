d68483b07e3bf5f606d71767cdf12c7144ac69d8
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-10 20:08:42 -0700

    Adding currentEmployee actions including set employee, login, and get current employee

---
+++ src/actions/currentEmployee.js
@@ -0,0 +1,39 @@
+export const setCurrentEmployee = employee => {
+  return {
+    type: "SET_CURRENT_EMPLOYEE",
+    employee
+  }
+}
+
+export const login = (credentials, history) => {
+  debugger
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/employees/login", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(resp => resp.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentEmployee(response.data))
+          debugger
+          // dispatch(getMyTasks)
+          console.log("signed in!")
+          history.pushState("/")
+        }
+      })
+    // .catch(alert) 
+  }
+}
+
+export const getCurrentEmployee = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/employees/get_current_employee")
+  }
+}
