23a198cd6fcd8b63a736a7489c3336bda56a75ba
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-12 15:06:33 -0700

    Added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logout from '../components/Logout.js'
+
+
+const NavBar = props => {
+
+  return (
+    <div className="NavBar">
+      <h2 className="center">Task Assign</h2>
+      {props.loggedIn ? < Logout /> : null}
+
+    </div>
+  )
+
+}
+
+export default NavBar
