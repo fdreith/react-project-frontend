88e3cd102fbf202470f151f2f41d73122d1c7d43
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-12 15:33:53 -0700

    Adding BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store'
 
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
