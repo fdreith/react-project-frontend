43a81225dba346560fc838e95e5df66ea738b1e1
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-12 15:33:03 -0700

    Adding fetch departments to the App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserLogin from "./components/UserLogin.js"
+import { connect } from 'react-redux';
+import { getCurrentUser } from './actions/currentUser'
+import Home from './containers/Home.js'
+import NavBar from './components/NavBar.js'
+import { fetchDepartments } from './actions/departments'
+import LoginForm from './components/LoginForm.js'
+import SignUpForm from './components/SignUpForm.js'
+
 
 
 class App extends Component {
 
+  componentDidMount() {
+    this.props.getCurrentUser()
+    this.props.fetchDepartments()
+  }
+
   render() {
+    console.log(this.props.loggedIn)
     return (
       <div className="App">
-        <h2>Task Assign</h2>
-        <UserLogin />
-  
+        <NavBar loggedIn={this.props.loggedIn} />
+        {this.props.loggedIn ? <Home /> : <><LoginForm /><br></br><br></br><SignUpForm departments={this.props.departments} /></>}
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser,
+    departments: state.departments
+  })
+}
+
+export default connect(mapStateToProps, { getCurrentUser, fetchDepartments })(App);
 
 
