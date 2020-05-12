d25a39bb2ca07b7c871d78fb44e0284322c31df6
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-12 15:35:38 -0700

    fixing typo

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,7 +1,15 @@
+// import { getMyTasks, clearTasks} from "./myTasks.js"
+
 export const setCurrentUser = user => {
   return {
     type: "SET_CURRENT_USER",
     user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
   }
 }
 
@@ -21,14 +29,79 @@ export const login = (credentials, history) => {
         if (response.error) {
           alert(response.error)
         } else {
-          debugger
-          // dispatch(setCurrentUser(response.data))
+          // debugger
+          dispatch(setCurrentUser(response))
           // dispatch(getMyTasks)
           console.log("signed in!")
-          history.pushState("/")
+          // history.pushState("/")
         }
       })
     // .catch(alert) 
+  }
+}
+
+export const signUp = (credentials, history) => {
+  debugger
+  return dispatch => {
+    const userInfo = {
+      user: credentials
+    }
+    return fetch("http://localhost:3001/api/v1/signup", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentUser(response.data))
+          // dispatch(getMyTasks())
+          history.push('/')
+        }
+      })
+      .catch(console.log)
+  }
+}
+
+
+export const logout = event => {
+  return dispatch => {
+    dispatch(clearCurrentUser())
+    // dispatch(clearTasks())
+    return fetch('http://localhost:3001/api/v1/logout', {
+      credentials: "include",
+      method: "DELETE"
+    })
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(response => {
+        if (response.error) {
+          // alert(response.error)
+          console.log(response.error)
+        } else {
+          dispatch(setCurrentUser(response))
+          // dispatch(getMyTasks())
+        }
+      })
+      .catch(console.log)
   }
 }
 
