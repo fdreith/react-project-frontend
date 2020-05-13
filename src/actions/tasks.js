3a424b67d66f772ca5a3e8f01a4a29d40da66240
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-13 14:59:54 -0700

    Adding fetchtasks and set tasks actions

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,3 +1,54 @@
+export const fetchTasks = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/tasks", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then(resp => resp.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          debugger
+          dispatch(setTasks(response))
+        }
+      })
+    // .catch(alert) 
+  }
+}
+
+export const setTasks = tasks => {
+  return {
+    type: "SET_TASKS",
+    tasks
+  }
+}
+
+export const postTask = task => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(task)
+    })
+      .then(resp => resp.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          debugger
+          dispatch(addTask(response))
+        }
+      })
+    // .catch(alert) 
+  }
+}
+
 export const addTask = task => {
   return {
     type: "ADD_TASK",
