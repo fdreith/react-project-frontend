0e91d249f1f296613ba2d46ad86bc79fdf3039a3
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 14:07:12 -0700

    Adding conditional rendering

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,11 +7,17 @@ import TaskForm from './TaskForm.js'
 class Home extends Component {
 
   render() {
+    let myTasksComponent
+    if (Object.keys(this.props.tasks).length === 0) {
+      myTasksComponent = <MyTasks />
+    } else {
+      myTasksComponent = "You have no tasks yet."
+    }
     return (
       <div>
         <h4>{this.props.currentUser.attributes.name}'s Tasks:</h4>
         <>
-          <MyTasks />
+          {myTasksComponent}
           <TaskForm users={this.props.users} currentUser={this.props.currentUser} />
         </>
       </div>
@@ -22,7 +28,8 @@ class Home extends Component {
 const mapStateToProps = state => {
   return ({
     currentUser: state.currentUser,
-    users: state.users
+    users: state.users,
+    tasks: state.tasks
   })
 }
 
