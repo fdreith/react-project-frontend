f6effcb86e1446100cb306b2a6aa52edcd11cf3c
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-13 17:56:33 -0700

    mapping to Task.js component

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import Task from './Task.js'
 
 
 
 const MyTasks = props => {
 
 
-    return (
-      <div>
-       
-      </div>
-    )
-  
+  return (
+    <div>
+      {/* <Task task={props.tasks.map(task => task)} /> */}
+
+    </div>
+  )
+
 }
 
 export default MyTasks
