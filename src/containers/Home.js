789d1a60b897189120aa77a23bfdfee0fa723581
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 11:14:26 -0700

    deleting logic to filter assigned tasks and my tasks

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,30 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import MyTasks from '../components/MyTasks.js'
 import TaskForm from './TaskForm.js'
-import { fetchTasks } from '../actions/tasks'
 
 
 class Home extends Component {
 
-  componentDidMount() {
-    this.props.fetchTasks()
-  }
-
-  filterMyTasks = () => {
-    return this.props.tasks.filter(task => task.relationships.user.data.id === this.props.currentUser.id)
-  }
-
-  filterAssignedTasks = () => {
-    return this.props.tasks.filter(task => task.relationships.owner.data.id === this.props.currentUser.id).filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id)
-  }
-
   render() {
     return (
       <div>
-        {console.log(this.props)}
         <h4>{this.props.currentUser.attributes.name}'s Tasks:</h4>
         <>
-          <MyTasks currentUser={this.props.currentUser} myTasks={this.filterMyTasks()} assignedTasks={this.filterAssignedTasks()} />
+          <MyTasks />
           <TaskForm users={this.props.users} currentUser={this.props.currentUser} />
         </>
       </div>
@@ -36,9 +22,8 @@ class Home extends Component {
 const mapStateToProps = state => {
   return ({
     currentUser: state.currentUser,
-    users: state.users,
-    tasks: state.tasks
+    users: state.users
   })
 }
 
-export default connect(mapStateToProps, { fetchTasks })(Home)
+export default connect(mapStateToProps)(Home)
