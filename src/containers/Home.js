4ed131b13f09b2320986e41d3367977a00071623
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-13 17:57:46 -0700

    adding functions to filter tasks data

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import MyTasks from '../components/MyTasks.js'
 import TaskForm from './TaskForm.js'
+import { fetchTasks } from '../actions/tasks'
 
 
-const Home = props => {
+class Home extends Component {
 
-  return (
-    <div>
-      {console.log(props)}
-      <h4>{props.currentUser.name}'s Tasks:</h4>
-      <>
-        <MyTasks currentUser={props.currentUser} />
-        <TaskForm users={props.users} currentUser={props.currentUser} />
-      </>
-    </div>
-  )
+  componentDidMount() {
+    this.props.fetchTasks()
+  }
+
+  filterTasks = () => {
+    const assignedTasks = this.props.tasks.filter(task => task.relationships.owner.data.id === this.props.currentUser.id)
+    const myTasks = this.props.tasks.filter(task => task.relationships.user.data.id === this.props.currentUser.id)
+    debugger
+
+  }
+
+  render() {
+    return (
+      <div>
+        {console.log(this.props)}
+        <h4>{this.props.currentUser.attributes.name}'s Tasks:</h4>
+        <>
+          <MyTasks currentUser={this.props.currentUser} tasks={this.filterTasks()} />
+          <TaskForm users={this.props.users} currentUser={this.props.currentUser} />
+        </>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return ({
     currentUser: state.currentUser,
-    users: state.users
+    users: state.users,
+    tasks: state.tasks
   })
 }
 
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps, { fetchTasks })(Home)
