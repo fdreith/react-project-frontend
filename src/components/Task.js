5a4ad71d1facb5baad803d8baffa9c303c4ead3e
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 14:09:57 -0700

    deleting map

---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+const Task = props => {
+
+
+  return (
+    <div>
+      {console.log(props.task.attributes.content)}
+
+    </div>
+  )
+
+}
+
+export default Task
