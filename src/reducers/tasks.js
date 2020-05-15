4266cb303a831c3946a8badb665538db37d72cd2
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 15:40:00 -0700

    adding all tasks

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,20 +1,20 @@
 export default (state = {
-  myTasks: [{
-    id: "",
-    type: "",
-    attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
-  }], assignedTasks: [{
-    id: "",
-    type: "",
-    attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
-  }]
+  myTasks: [
+    // {
+    // id: "",
+    // type: "",
+    // attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
+    // }
+  ], assignedTasks: [], allTasks: []
 }, action) => {
 
   switch (action.type) {
     case 'SET_TASKS':
-      const assignedTasks = convertDates(action.tasks.filter(task => task.type === "assigned_task").filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id))
-      const myTasks = convertDates(action.tasks.filter(task => task.type === "task"))
-      return { myTasks, assignedTasks }
+      const allTasks = action.tasks
+      const assignedTasks = action.tasks.filter(task => task.type === "assigned_task").filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id)
+      const myTasks = action.tasks.filter(task => task.type === "task")
+      console.log(assignedTasks)
+      return { myTasks, assignedTasks, allTasks }
     case 'ADD_TASK':
       debugger
       if (action.task.relationships.user.data.id === action.task.relationships.owner.data.id) {
