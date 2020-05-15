f301ed18bac2b94bc41eef07d0c42ff4631e8b4d
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 20:05:07 -0700

    trying out accordin styling from bootstrap and adding state to pass down to children

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import TaskShow from './TaskShow'
+// import Accordion from 'react-bootstrap/Accordion'
+// import Card from 'react-bootstrap/Card'
+// import Comment from './Comment.js'
+// import CommentForm from './CommentForm.js'
 
 
+const Task = (props) => {
 
-
-const Task = props => {
-
+  let taskInfo
+  const handleClick = event => {
+    console.log("clicked", props.task.attributes.id || "")
+    return taskInfo = <TaskShow task={props.task} />
+  }
 
   return (
     <div>
-      {console.log(props.task.attributes.content)}
-
+      <li onClick={handleClick} name={props.task}>
+        {props.task.attributes.content || "You have no tasks"}
+        {taskInfo}
+      </li>
     </div>
   )
+
 
 }
 
