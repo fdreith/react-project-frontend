8ca736070fda7200df298c0118560227c3c32b14
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 10:53:56 -0700

    fixing error of not getting props and showing more info

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TaskShow from './TaskShow'
 // import Accordion from 'react-bootstrap/Accordion'
 // import Card from 'react-bootstrap/Card'
@@ -6,22 +6,41 @@ import TaskShow from './TaskShow'
 // import CommentForm from './CommentForm.js'
 
 
-const Task = (props) => {
-
-  let taskInfo
-  const handleClick = event => {
-    console.log("clicked", props.task.attributes.id || "")
-    return taskInfo = <TaskShow task={props.task} />
+class Task extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showComponent: false
+    }
   }
 
-  return (
-    <div>
-      <li onClick={handleClick} name={props.task}>
-        {props.task.attributes.content || "You have no tasks"}
-        {taskInfo}
-      </li>
-    </div>
-  )
+
+  handleClick = event => {
+    this.state.showComponent === false ?
+      this.setState({
+        showComponent: true
+      })
+      :
+      this.setState({
+        showComponent: false
+      })
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <li onClick={this.handleClick}>
+          {this.props.task.attributes.content} - {this.props.task.attributes.due_date}
+          {/* {this.props.task.attributes.content || "You have no tasks"} */}
+        </li>
+        {this.state.showComponent ?
+          <TaskShow task={this.props.task} /> : null
+        }
+      </div>
+    )
+  }
 
 
 }
--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import Comment from './Comment.js'
 import CommentForm from './CommentForm.js'
+import Card from 'react-bootstrap/Card'
+import { deleteTask } from '../actions/tasks'
+import { connect } from 'react-redux'
 
 
 const TaskShow = ({ task }) => {
 
   return (
-    task
-      ? <div>
-        <p>Due Date: {task.attributes.due_date}</p>
-        <p>Assigned from:{task.attrubutes.owner.name}</p>
-        <ul>
-          {task.attributes.comments.map(comment => <Comment comment={comment} />)}
-          <CommentForm taskId={task.id} />
+    <div>
+      <Card>
+        <button onClick={props.deleteTask(task.attributes.id)}>X</button>
+        <p>Assigned by: {task.attributes.owner.name}</p>
+        {task.attributes.comments.map(comment => <Comment key={comment.id} comment={comment} />)}
 
-        </ul>
-      </div>
-      : <p>task show page</p>
+        {/* <CommentForm taskId={task.attributes.id} /> */}
+      </Card>
+    </div>
+
+
   )
 
 }
 
-export default TaskShow
+export default connect(null, { deleteTask })(TaskShow)
