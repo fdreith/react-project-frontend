533262367f4bc217b9ce68b0b91bebae2be2f952
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 12:44:25 -0700

    cleaning up

---
+++ src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { login } from '../actions/currentUser'
+import { connect } from 'react-redux'
+
+
+class LoginForm extends Component {
+
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.login(this.state)
+    this.setState = ({
+      email: "",
+      password: ""
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Login:   </label>
+        <br></br>
+        <input placeholder="email" value={this.state.email} name="email" type="text" onChange={this.handleChange} />
+        <input placeholder="password" value={this.state.password} name="password" type="text" onChange={this.handleChange} />
+        <input type="submit" value="Log In" />
+      </form>
+    )
+  }
+}
+
+export default connect(null, { login })(LoginForm)
+
+
+
+
