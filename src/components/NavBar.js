5ca3a292d927999e1da7317360e511ecf7a4eb9b
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 10:49:38 -0700

    Adding routes in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logout from '../components/Logout.js'
+import { NavLink } from 'react-router-dom'
 
 
 const NavBar = props => {
@@ -7,7 +8,14 @@ const NavBar = props => {
   return (
     <div className="NavBar">
       <h2 className="center">Task Assign</h2>
-      {props.loggedIn ? < Logout /> : null}
+      {props.loggedIn ?
+        <>
+          <NavLink to="/tasks"> My Tasks </NavLink>
+          <NavLink to="/tasks/assigned"> Assigned Tasks </NavLink>
+          <NavLink to="/tasks/completed"> Completed Tasks </NavLink>
+          < Logout />
+        </>
+        : null}
     </div>
   )
 
