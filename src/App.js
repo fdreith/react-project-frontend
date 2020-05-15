d39b4c35ce0207d90bbff61b2f2bff6ecbeb39cd
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 20:02:28 -0700

    messing with the layout and columns

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser'
-import Home from './containers/Home.js'
+import Home from './containers/TasksContainer.js'
 import NavBar from './components/NavBar.js'
 import { fetchDepartments } from './actions/departments'
 import LoginForm from './containers/LoginForm.js'
 import SignUpForm from './containers/SignUpForm.js'
+import Container from 'react-bootstrap/Container'
+
 
 class App extends Component {
 
@@ -19,9 +21,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar loggedIn={this.props.loggedIn} />
-        {this.props.loggedIn ? <Home /> : <><LoginForm /><br></br><br></br><SignUpForm departments={this.props.departments} /></>}
-
+        <Container fluid>
+          <NavBar loggedIn={this.props.loggedIn} />
+          {this.props.loggedIn ? <Home /> : <><LoginForm /><br></br><br></br><SignUpForm departments={this.props.departments} /></>}
+        </Container >
       </div>
     );
   }
