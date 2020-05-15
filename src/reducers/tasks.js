3ff32bb4fc21573a965627695ea79aedca0d3dfe
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-14 20:03:56 -0700

    playing with initial state values to fix undefined props issue

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,11 +1,24 @@
-export default (state = { myTasks: [], assignedTasks: [] }, action) => {
+export default (state = {
+  myTasks: [{
+    id: "",
+    type: "",
+    attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
+  }], assignedTasks: [{
+    id: "",
+    type: "",
+    attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
+  }]
+}, action) => {
   switch (action.type) {
     case 'SET_TASKS':
-      const assignedTasks = action.tasks.filter(task => task.type === "assigned_task")
+      const assignedTasks = action.tasks.filter(task => task.type === "assigned_task").filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id)
       const myTasks = action.tasks.filter(task => task.type === "task")
       return { myTasks, assignedTasks }
     case 'ADD_TASK':
-      return [...state, action.task]
+      debugger
+      if (action.task.relationships.user.data.id === action.task.relationships.owner.data.id) {
+        return { ...state, myTasks: [...action.task] }
+      }
     case 'DELETE_TASK':
       return state.filter(task => task.id !== action.id)
     default:
