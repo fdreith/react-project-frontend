5e33ea350ce9a6699ea4a10bb5a15bdebcec3e16
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 17:20:10 -0700

    update tasks reducer

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -10,18 +10,36 @@ export default (state = {
 
   switch (action.type) {
     case 'SET_TASKS':
-      const allTasks = action.tasks
-      const assignedTasks = action.tasks.filter(task => task.type === "assigned_task").filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id)
-      const myTasks = action.tasks.filter(task => task.type === "task")
-      console.log(assignedTasks)
-      return { myTasks, assignedTasks, allTasks }
+      const tasks = convertDates(action.tasks)
+      const assignedTasks = tasks.filter(task => task.type === "assigned_task").filter(task => task.relationships.user.data.id !== task.relationships.owner.data.id)
+      const myTasks = tasks.filter(task => task.type === "task")
+      return { myTasks, assignedTasks }
     case 'ADD_TASK':
-      debugger
-      if (action.task.relationships.user.data.id === action.task.relationships.owner.data.id) {
+      if (myTasks(action)) {
         return { ...state.myTasks, ...action.task }
       } else {
         return { ...state.assignedTasks, ...action.task }
       }
+    case 'UPDATE_TASK':
+      debugger
+      if (myTasks(action)) {
+        return state.myTasks.map(task => {
+          if (task.id === action.task.id) {
+            return action.task
+          } else {
+            return task
+          }
+        })
+      } else {
+        return state.assignedTasks.map(task => {
+          if (task.id === action.task.id) {
+            return action.task
+          } else {
+            return task
+          }
+        })
+      }
+
     case 'DELETE_TASK':
       return state.filter(task => task.id !== action.id)
     default:
@@ -30,5 +48,12 @@ export default (state = {
 }
 
 const convertDates = (tasks) => {
-  tasks.map(task => task.attributes.due_date = new Date(task.attributes.due_date))
+  return tasks.map(task => {
+    task.attributes.due_date = new Date(task.attributes.due_date)
+    return task
+  })
+}
+
+const myTasks = (action) => {
+  return action.task.relationships.user.data.id === action.task.relationships.owner.data.id
 }
