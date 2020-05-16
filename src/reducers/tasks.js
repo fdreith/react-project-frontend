b4fbd2c952a216e357b135931e3b32889a2932cb
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-15 21:50:06 -0700

    fixing add and update actions

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -5,7 +5,7 @@ export default (state = {
     // type: "",
     // attributes: { id: "", content: "", due_date: "", completed: "", comments: [] }, user: "", owner: ""
     // }
-  ], assignedTasks: [], allTasks: []
+  ], assignedTasks: []
 }, action) => {
 
   switch (action.type) {
@@ -15,32 +15,34 @@ export default (state = {
       const myTasks = tasks.filter(task => task.type === "task")
       return { myTasks, assignedTasks }
     case 'ADD_TASK':
-      if (myTasks(action)) {
-        return { ...state.myTasks, ...action.task }
+      if (isMyTasks(action)) {
+        return { ...state, myTasks: [...state.myTasks, action.task] }
       } else {
-        return { ...state.assignedTasks, ...action.task }
+        return { ...state, assignedTasks: [...state.assignedTasks, action.task] }
       }
     case 'UPDATE_TASK':
-      debugger
       if (isMyTasks(action)) {
-        return state.myTasks.map(task => {
+        const newMyTasksState = state.myTasks.map(task => {
           if (task.id === action.task.id) {
             return action.task
           } else {
             return task
           }
         })
+        return { ...state, myTasks: newMyTasksState }
       } else {
-        return state.assignedTasks.map(task => {
+        const newAssignedTasksState = state.assignedTasks.map(task => {
           if (task.id === action.task.id) {
             return action.task
           } else {
             return task
           }
         })
+        return { ...state, assignedTasks: newAssignedTasksState }
       }
 
     case 'DELETE_TASK':
+      debugger
       return state.filter(task => task.id !== action.id)
     default:
       return state
