2bc19d32768b4386ac95095a2eb43760a8518519
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-16 18:47:16 -0700

    displaying date and time

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -7,14 +7,25 @@ const Comment = (props) => {
   const findUser = userId => {
     return props.users.find(user => user.id === userId)
   }
+
   return (
     <div>
       <p>
-        {findUser(props.comment.user_id).name}: {props.comment.content} at {props.comment.created_at}
+        {findUser(props.comment.user_id).name}: {props.comment.content} at {displayDateAndTime(props.comment.created_at)}
       </p>
       {console.log(props)}
     </div>
   )
+}
+
+const displayDateAndTime = (dateString) => {
+  debugger
+  const date = new Date(dateString)
+  const month = date.getMonth() + 1
+  const hour = date.getHours()
+  const min = date.getMinutes()
+  return `${hour}:${min} on ${month}-${date.getDate()}-${date.getFullYear()}`
+
 }
 const mapStateToProps = state => {
   return ({
