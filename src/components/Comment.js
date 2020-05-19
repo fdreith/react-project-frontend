081aafbf268a7c87d06178670b378d4f6d611f64
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-19 14:29:35 -0700

    fixing some js formmating from the icons

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -11,9 +11,9 @@ const Comment = (props) => {
       <p>
         {props.comment.attributes.user.name} at {displayDateAndTime(props.comment.attributes.created_at)} : {props.comment.attributes.content}
         {props.comment.attributes.user.id === parseInt(props.currentUser.attributes.id) && <Button variant="light" size="sm" onClick={() => props.deleteComment(props.comment.attributes.id)}>
-          <svg class="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd" />
-            <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd" />
+          <svg className="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clipRule="evenodd" />
+            <path fillRule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clipRule="evenodd" />
           </svg>
         </Button>}
       </p>
