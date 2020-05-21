07b6c1f58003fce87859bd2991de5946e281610b
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-20 19:03:24 -0700

    changing it back to funtion component

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,40 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Task from './Task.js'
-import Button from 'react-bootstrap/Button'
 
 
-class MyTasks extends Component {
+const MyTasks = props => {
 
-  state = {
-    counter: 0
-  }
-
-  handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-
-
-  render() {
-
-    return (
-      <div className="myTasks">
-        <h6>{this.props.currentUser.attributes.name}'s Tasks:</h6>
-        <ul>
-          {this.props.myTasks && this.props.myTasks.map(task => {
-            return (
-              <div key={task.id}>
-                <Task task={task} history={this.props.history} />
-               
-              </div>)
-          })}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="myTasks">
+      <h6>{props.currentUser.attributes.name}'s Tasks:</h6>
+      <ul>
+        {props.myTasks && props.myTasks.map(task => {
+          return (
+            <div key={task.id}>
+              <Task task={task} history={props.history} />
+            </div>)
+        })}
+      </ul>
+    </div>
+  )
 }
 
-
 export default MyTasks
-
