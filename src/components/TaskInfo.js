a9b6351091797c6937081da46b43c40976a99d11
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-11 22:23:30 -0700

    add spacing to taskInfo

--- src/components/TaskInfo.js
+++ src/components/TaskInfo.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react'
-import Comment from './Comment.js'
-import Card from 'react-bootstrap/Card'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { updateTask } from '../actions/tasks'
-import CommentForm from './CommentForm.js'
-import Button from 'react-bootstrap/Button'
+import React, { Component } from "react";
+import Comment from "./Comment.js";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { updateTask } from "../actions/tasks";
+import CommentForm from "./CommentForm.js";
+import Button from "react-bootstrap/Button";
 
 class TaskInfo extends Component {
-
   state = {
     content: this.props.task.attributes.content,
     due_date: this.props.task.attributes.due_date,
     user_id: this.props.task.attributes.user.id,
     owner_id: this.props.task.attributes.owner.id,
-    completed: true
-  }
+    completed: true,
+  };
 
   handleClick = () => {
-    this.props.updateTask(this.state, this.props.task.attributes.id, this.props.history, "completed")
-  }
+    this.props.updateTask(
+      this.state,
+      this.props.task.attributes.id,
+      this.props.history,
+      "completed"
+    );
+  };
 
   render() {
     return (
@@ -27,38 +31,83 @@ class TaskInfo extends Component {
         <br />
         <Card>
           <Card.Body>
-            {this.props.completed ||
+            {this.props.completed || (
               <Button variant="outline-secondary" onClick={this.handleClick}>
-                <svg className="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clipRule="evenodd" />
+                <svg
+                  className="bi bi-check"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z"
+                    clipRule="evenodd"
+                  />
                 </svg>
-                  Complete
+                Complete
               </Button>
-            }
+            )}
             <br></br>
-            <p>Assigned to: {this.props.task.attributes.user.name} by {this.props.task.attributes.owner.name}</p>
+            <p>
+              Assigned to: {this.props.task.attributes.user.name} by{" "}
+              {this.props.task.attributes.owner.name}
+            </p>
             <h6>Task Comments:</h6>
-            {this.props.comments.map(comment => comment.attributes.task_id === this.props.task.attributes.id && <Comment key={comment.attributes.id} comment={comment} currentUser={this.props.currentUser} />)}
-            <CommentForm taskId={this.props.task.attributes.id} currentUser={this.props.currentUser} />
-            {!this.props.completed &&
+            {this.props.comments.map(
+              (comment) =>
+                comment.attributes.task_id ===
+                  this.props.task.attributes.id && (
+                  <Comment
+                    key={comment.attributes.id}
+                    comment={comment}
+                    currentUser={this.props.currentUser}
+                  />
+                )
+            )}
+            <CommentForm
+              taskId={this.props.task.attributes.id}
+              currentUser={this.props.currentUser}
+            />
+            {!this.props.completed && (
               <Link to={`/tasks/${this.props.task.attributes.id}/edit`}>
-                <svg className="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clipRule="evenodd" />
-                  <path fillRule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clipRule="evenodd" />
+                <svg
+                  className="bi bi-pencil"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"
+                    clipRule="evenodd"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z"
+                    clipRule="evenodd"
+                  />
                 </svg>
-              </Link>}
+              </Link>
+            )}
           </Card.Body>
         </Card>
+        <br />
+        <br />
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return ({
+const mapStateToProps = (state) => {
+  return {
     comments: state.comments,
-    currentUser: state.currentUser
-  })
-}
+    currentUser: state.currentUser,
+  };
+};
 
-export default connect(mapStateToProps, { updateTask })(TaskInfo)
+export default connect(mapStateToProps, { updateTask })(TaskInfo);
