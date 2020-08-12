1ebbe9f475ae557cbba3d97a5bb4380d3c134ca2
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-12 16:27:40 -0700

    switch from a functional component to a class component

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import Task from './Task.js'
+import React, { Component } from "react";
+import Task from "./Task.js";
+import TaskInfo from "./TaskInfo.js";
 
+class MyTasks extends Component {
+  state = {
+    showComponent: true,
+    task: "",
+  };
 
-const MyTasks = (props) => {
+  toggleTaskInfo = () => {
+    debugger;
+    this.setStaet((state) => ({
+      showComponent: !state.showComponent,
+    }));
+  };
 
-  return (
-    < div className="myTasks" >
-      <h6>{props.currentUser.attributes.name}'s Tasks:</h6>
-      <ul>
-        {props.myTasks.map(task => {
-          return (
-            <div key={task.id}>
-              <Task task={task} history={props.history} />
-            </div>)
-        })}
-      </ul>
-    </div >
-  )
-
+  render() {
+    return (
+      <div className="myTasks">
+        <div className="row">
+          <div className="col-xs-4">
+            <h6>{this.props.currentUser.attributes.name}'s Tasks:</h6>
+            <ul>
+              {this.props.myTasks.map((task) => {
+                return (
+                  <div key={task.id}>
+                    <Task
+                      task={task}
+                      history={this.props.history}
+                      toggleTaskInfo={this.toggleTaskInfo}
+                    />
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="col-xs-4">
+            {this.state.showComponent && (
+              <TaskInfo task={this.state.task} history={this.props.history} />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MyTasks
+export default MyTasks;
