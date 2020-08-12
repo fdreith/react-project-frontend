3fd297d4e96ff5c23c972a93b25e1c5f4522d465
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-11 23:30:37 -0700

    remove unecessary css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import TaskInfo from './TaskInfo'
+import React, { Component } from "react";
+import TaskInfo from "./TaskInfo";
 
 class Task extends Component {
   state = {
     showComponent: false,
-  }
+  };
 
-  handleClick = event => {
+  handleClick = (event) => {
     this.setState({
       showComponent: !this.state.showComponent,
-    })
-  }
+    });
+  };
 
   renderTask = () => {
     if (this.props.completed) {
@@ -19,66 +19,89 @@ class Task extends Component {
           <li onClick={this.handleClick}>
             {this.props.task.attributes.content}
           </li>
-          {this.state.showComponent &&
-            <TaskInfo task={this.props.task} history={this.props.history} completed={this.props.completed} />
-          }
+          {this.state.showComponent && (
+            <TaskInfo
+              task={this.props.task}
+              history={this.props.history}
+              completed={this.props.completed}
+            />
+          )}
         </div>
-      )
+      );
     } else {
       return (
-        <div className="row">
+        <>
+          <div className="row">
+            <div className="col-xs-4">
+              <li
+                className={
+                  this.props.task.attributes.due_date > new Date()
+                    ? "text-default"
+                    : "text-danger"
+                }
+                onClick={this.handleClick}
+              >
+                {this.props.task.attributes.content} - by{" "}
+                {todayOrTomorrow(this.props.task.attributes.due_date) ||
+                  displayDate(this.props.task.attributes.due_date)}
+              </li>
+            </div>
           <div className="col-xs-4">
-          </div>
-          <div className="col-xs-4">
-            <li
-              className={this.props.task.attributes.due_date > new Date() ? "text-default" : "text-danger"}
-              onClick={this.handleClick}>
-              {this.props.task.attributes.content} - by {todayOrTomorrow(this.props.task.attributes.due_date) || displayDate(this.props.task.attributes.due_date)
-              }
-            </li>
-            {this.state.showComponent &&
+            {this.state.showComponent && (
               <TaskInfo task={this.props.task} history={this.props.history} />
-            }
+            )}
           </div>
-        </div>
-      )
+          </div>
+        </>
+      );
     }
-  }
+  };
 
   render() {
-    return (
-      <div>
-        {this.renderTask()}
-      </div>
-    )
-
+    return <div>{this.renderTask()}</div>;
   }
 }
 
 const todayOrTomorrow = (date) => {
-  if (date.getFullYear() === new Date().getFullYear() &&
+  if (
+    date.getFullYear() === new Date().getFullYear() &&
     date.getMonth() === new Date().getMonth() &&
-    date.getDate() === new Date().getDate()) {
-    return "Today"
-  } else if (date.getFullYear() === new Date().getFullYear() &&
+    date.getDate() === new Date().getDate()
+  ) {
+    return "Today";
+  } else if (
+    date.getFullYear() === new Date().getFullYear() &&
     date.getMonth() === new Date().getMonth() &&
-    date.getDate() === (new Date().getDate() + 1)) {
-    return "Tomorrow"
+    date.getDate() === new Date().getDate() + 1
+  ) {
+    return "Tomorrow";
   } else {
-    return false
+    return false;
   }
-}
-
+};
 
 const displayDate = (dateString) => {
-  const date = new Date(dateString)
-  const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-  const weekday = days[date.getDay()]
-  const month = months[date.getMonth()]
-  const day = date.getDate()
-  const year = date.getFullYear()
-  return `${weekday}, ${month} ${day} ${year}`
-}
+  const date = new Date(dateString);
+  const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const weekday = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${weekday}, ${month} ${day} ${year}`;
+};
 
-export default Task
+export default Task;
