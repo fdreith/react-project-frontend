86e361e4cf8149440b57d4c88c9349bb97eda061
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-13 20:11:14 -0700

    changing from a class component to a functional

--- src/components/AssignedTasks.js
+++ src/components/AssignedTasks.js
@@ -1,42 +1,22 @@
 import React, { Component } from "react";
 import Task from "./Task.js";
-import TaskInfo from "./TaskInfo.js";
 
-class AssignedTasks extends Component {
-  state = {
-    showComponent: false,
-    task: "",
-  };
-
-  showComponent = (event) => {
-    const taskId = event.target.attributes.id.nodeValue;
-    const task = this.props.myTasks.find((task) => task.id === taskId);
-    this.setState((state) => ({
-      showComponent: !state.showComponent,
-      task: task,
-    }));
-  };
-  render() {
-    return (
-      <div className="assignedTasks">
-        <div className="row">
-          <div className="col-xs-4">
-            <h6>Assigned Tasks:</h6>
-            <ul>
-              {this.props.assignedTasks.map((task) => (
-                <Task key={task.id} task={task} history={this.props.history} />
-              ))}
-            </ul>
-          </div>
-          <div className="col-xs-4">
-            {this.state.showComponent && (
-              <TaskInfo task={this.state.task} history={this.props.history} />
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const AssignedTasks = (props) => {
+  return (
+    <div className="assignedTasks">
+      <h6>Assigned Tasks:</h6>
+      <ul>
+        {props.assignedTasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            history={props.history}
+            showComponent={props.showComponent}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default AssignedTasks;
