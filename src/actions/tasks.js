3615cdb7e6b9aa019d2f26e0697a3bf5986be333
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-20 21:03:55 -0700

    clean up code

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,92 +1,90 @@
-
-export const setTasks = tasks => {
+export const setTasks = (tasks) => {
   return {
     type: "SET_TASKS",
-    tasks
-  }
-}
+    tasks,
+  };
+};
 
-export const postTask = task => {
-  return dispatch => {
+export const postTask = (task) => {
+  return (dispatch) => {
     return fetch("http://localhost:3001/api/v1/tasks", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(task)
+      body: JSON.stringify(task),
     })
-      .then(resp => resp.json())
-      .then(response => {
+      .then((resp) => resp.json())
+      .then((response) => {
         if (response.content) {
-          alert(response.content)
+          alert(response.content);
         } else {
-          dispatch(addTask(response.data))
+          dispatch(addTask(response.data));
         }
       })
-      .catch(alert)
-  }
-}
+      .catch(alert);
+  };
+};
 
-export const addTask = task => {
+export const addTask = (task) => {
   return {
     type: "ADD_TASK",
-    task
-  }
-}
+    task,
+  };
+};
 
 export const updateTask = (task, id, history, completed) => {
-  return dispatch => {
+  return (dispatch) => {
     return fetch(`http://localhost:3001/api/v1/tasks/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(task)
+      body: JSON.stringify(task),
     })
-      .then(resp => resp.json())
-      .then(response => {
+      .then((resp) => resp.json())
+      .then((response) => {
         if (response.content) {
-          alert(response.content)
+          alert(response.content);
         } else {
-          completed === "incomplete" && history.go(-1)
-          dispatch(updateTaskStore(response.data))
+          dispatch(updateTaskStore(response.data));
         }
-      })
+      });
     // .catch(alert)
-  }
-}
+  };
+};
 
-export const updateTaskStore = task => {
+export const updateTaskStore = (task) => {
   return {
     type: "UPDATE_TASK",
-    task
-  }
-}
+    task,
+  };
+};
 
 export const deleteTask = (taskId, history) => {
-  history.go(-1)
-  return dispatch => {
+  history.go(-1);
+  return (dispatch) => {
     return fetch(`http://localhost:3001/api/v1/tasks/${taskId}`, {
       method: "DELETE",
     })
-      .then(resp => resp.json())
-      .then(response => {
+      .then((resp) => resp.json())
+      .then((response) => {
         if (response.message) {
           // alert(response.message)
-          dispatch(deleteTaskStore(taskId))
+          dispatch(deleteTaskStore(taskId));
         } else {
-          throw new Error(response.errors)
+          throw new Error(response.errors);
         }
-      })
+      });
     // .catch(alert)
-  }
-}
+  };
+};
 
-export const deleteTaskStore = taskId => {
+export const deleteTaskStore = (taskId) => {
   return {
     type: "DELETE_TASK",
-    taskId
-  }
-}
+    taskId,
+  };
+};
