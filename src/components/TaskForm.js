64f110cb46b5c1f0cafeb40b91bd4dbe223ef37b
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-21 10:50:59 -0700

    add passing renderTaskForm and call on submission to close form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,57 +1,60 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { postTask } from '../actions/tasks'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { postTask } from "../actions/tasks";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 class TaskForm extends Component {
-
   state = {
     content: "",
     due_date: new Date(),
     user_id: this.props.currentUser.id,
-    owner_id: this.props.currentUser.id
-  }
+    owner_id: this.props.currentUser.id,
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     if (event.target.name === "user_id") {
       this.setState({
-        user_id: parseInt(event.target.value)
-      })
+        user_id: parseInt(event.target.value),
+      });
     } else {
       this.setState({
-        [event.target.name]: event.target.value
-      })
+        [event.target.name]: event.target.value,
+      });
     }
-  }
+  };
 
-  handleDateChange = event => {
+  handleDateChange = (event) => {
     this.setState({
-      due_date: event
-    })
-  }
+      due_date: event,
+    });
+  };
 
-  handleSubmit = event => {
-    event.preventDefault()
-    this.props.postTask(this.state)
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.postTask(this.state);
     this.setState({
       content: "",
       due_date: new Date(),
       user_id: this.props.currentUser.id,
-      owner_id: this.props.currentUser.id
-    })
-  }
+      owner_id: this.props.currentUser.id,
+    });
+    this.props.renderTaskForm();
+  };
 
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Group >
+        <Form.Group>
           <Form.Label>New Task:</Form.Label>
-          <Form.Control value={this.state.content} name="content" type="textarea" onChange={this.handleChange} />
+          <Form.Control
+            value={this.state.content}
+            name="content"
+            type="textarea"
+            onChange={this.handleChange}
+          />
         </Form.Group>
         <Form.Group>
           <Form.Label>Select Due Date:</Form.Label>
@@ -63,7 +66,7 @@ class TaskForm extends Component {
             dateFormat="MM/dd/yyyy"
           />
         </Form.Group>
-        <Form.Group >
+        <Form.Group>
           <Form.Label>Who is this task assigned to:</Form.Label>
           <Form.Control
             as="select"
@@ -71,26 +74,29 @@ class TaskForm extends Component {
             id="user-select"
             value={this.state.user_id}
             onChange={this.handleChange}
-            custom>
+            custom
+          >
             <option value={this.props.currentUser.id}>Choose a user</option>
-            {this.props.users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)}
+            {this.props.users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
           </Form.Control>
         </Form.Group>
-        <Button size="sm" variant="outline-secondary" type="submit">Add Task</Button>
+        <Button size="sm" variant="outline-secondary" type="submit">
+          Add Task
+        </Button>
       </Form>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return ({
+const mapStateToProps = (state) => {
+  return {
     currentUser: state.currentUser,
-    users: state.users
-  })
-}
+    users: state.users,
+  };
+};
 
-export default connect(mapStateToProps, { postTask })(TaskForm)
-
-
-
-
+export default connect(mapStateToProps, { postTask })(TaskForm);
