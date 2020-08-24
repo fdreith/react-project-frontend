501dec222c56880de7b062eb420dbd745b3467ff
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-23 21:56:49 -0700

    change comment form submit button to plane

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,46 +1,51 @@
-import React, { Component } from 'react'
-import { postComment } from '../actions/comments'
-import { connect } from 'react-redux'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
+import React, { Component } from "react";
+import { postComment } from "../actions/comments";
+import { connect } from "react-redux";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class CommentForm extends Component {
-
   state = {
-    content: ""
-  }
+    content: "",
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  handleSubmit = event => {
-    event.preventDefault()
+  handleSubmit = (event) => {
+    event.preventDefault();
     this.props.postComment({
-      ...this.state, task_id: this.props.taskId, user_id: parseInt(this.props.currentUser.id)
-    })
+      ...this.state,
+      task_id: this.props.taskId,
+      user_id: parseInt(this.props.currentUser.id),
+    });
     this.setState({
-      content: ""
-    })
-  }
+      content: "",
+    });
+  };
 
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="comment">
-          <Form.Control size="sm" type="text" name="content" value={this.state.content} placeholder="New comment.." onChange={this.handleChange} />
+          <Form.Control
+            size="sm"
+            type="text"
+            name="content"
+            value={this.state.content}
+            placeholder="New comment.."
+            onChange={this.handleChange}
+          />
         </Form.Group>
-        <Button size="sm" variant="outline-secondary" type="submit">Submit</Button>
+        <Button size="sm" variant="outline-secondary" type="submit">
+          <i class="fa fa-telegram"></i>
+        </Button>
       </Form>
-    )
+    );
   }
 }
 
-export default connect(null, { postComment })(CommentForm)
-
-
-
-
+export default connect(null, { postComment })(CommentForm);
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 const Task = (props) => {
   const renderTask = () => {
