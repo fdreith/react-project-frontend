0d986ccdede903653efa7333f9e303be4d7bd017
Francesca Dreith <francesca.dreith@gmail.com>
2020-08-30 18:56:18 -0700

    add spacing between navbar and tasks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { connect } from 'react-redux';
-import { getCurrentUser } from './actions/currentUser'
-import TasksContainer from './components/TasksContainer.js'
-import NavBar from './components/NavBar.js'
-import { fetchDepartments } from './actions/departments'
-import { fetchComments } from './actions/comments'
-import { withRouter } from 'react-router-dom';
-import AuthContainer from './components/AuthContainer'
-
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { connect } from "react-redux";
+import { getCurrentUser } from "./actions/currentUser";
+import TasksContainer from "./components/TasksContainer.js";
+import NavBar from "./components/NavBar.js";
+import { fetchDepartments } from "./actions/departments";
+import { fetchComments } from "./actions/comments";
+import { withRouter } from "react-router-dom";
+import AuthContainer from "./components/AuthContainer";
 
 class App extends Component {
-
   componentDidMount() {
-    this.props.getCurrentUser()
-    this.props.fetchDepartments()
-    this.props.fetchComments()
+    this.props.getCurrentUser();
+    this.props.fetchDepartments();
+    this.props.fetchComments();
   }
 
   render() {
     return (
       <div className="App">
         <NavBar loggedIn={this.props.loggedIn} history={this.props.history} />
-        {this.props.loggedIn ?
+        <br />
+        {this.props.loggedIn ? (
           <TasksContainer history={this.props.history} />
-          :
+        ) : (
           <AuthContainer history={this.props.history} />
-        }
-      </div >
+        )}
+      </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return ({
-    loggedIn: !!state.currentUser
-  })
-}
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: !!state.currentUser,
+  };
+};
 
-export default withRouter(connect(mapStateToProps, { getCurrentUser, fetchDepartments, fetchComments })(App))
-
-
+export default withRouter(
+  connect(mapStateToProps, { getCurrentUser, fetchDepartments, fetchComments })(
+    App
+  )
+);
